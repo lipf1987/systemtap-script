5d333d4ebd0cb4575a7c88406dfe58ca60a6641e
lipengfei <lipengfei@pinecone.net>
2019-03-19 10:43:46 +0800

    systemtap  script
    Signed-off-by: lipengfei <lipengfei@pinecone.net>

---
+++ thermal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: thermal.sh
+# 
+#         USAGE: ./thermal.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2018年08月20日 16:21
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+ZZ
+
